801757bded4712ce09fc002eae5d3902630f817b
Jeremy Magland <jeremy.magland@gmail.com>
2018-09-12 22:31:07 -0400

    remove -it

--- releasetools/publish/publish_conda.sh
+++ releasetools/publish/publish_conda.sh
@@ -5,4 +5,4 @@ set -e
 SOURCEDIR=$1
 
 docker pull magland/mldevel_publish
-docker run -v ~/.anaconda:/root/.anaconda -v $SOURCEDIR:/source -it magland/mldevel_publish conda
+docker run -v ~/.anaconda:/root/.anaconda -v $SOURCEDIR:/source magland/mldevel_publish conda
--- releasetools/publish/publish_npm.sh
+++ releasetools/publish/publish_npm.sh
@@ -5,4 +5,4 @@ set -e
 SOURCEDIR=$1
 
 docker pull magland/mldevel_publish
-docker run -v ~/.npmrc:/root/.npmrc -v $SOURCEDIR:/source -it magland/mldevel_publish npm
+docker run -v ~/.npmrc:/root/.npmrc -v $SOURCEDIR:/source magland/mldevel_publish npm
